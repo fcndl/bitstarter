adcb41eee9f1f21eb29c9b68c1fead951d32f9fc
facundolezcano <facundolezcano@gmail.com>
2013-07-07 20:05:44 +0000

    web.js reads from html

--- web.js
+++ web.js
@@ -2,9 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+var fs = require('fs')
+
+var buffer = new Buffer(fs.readFileSync('index.html'));
+
+app('/', function(request, response) {
+  response.send(buffer.toString());
 });
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
